52be56ff6f3da4ba98400403167878fd28750641
Reetam101 <reetamraj2@gmail.com>
2024-08-28 00:04:37 +0530

    working on the clerk authentication and organization

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,8 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+const OrganizationPage = () => {
+  return <div>Organization page</div>;
+};
+
+export default OrganizationPage;
